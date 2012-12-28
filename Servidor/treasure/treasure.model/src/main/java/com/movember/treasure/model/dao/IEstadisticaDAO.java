e449076f7554a628f145b689235b34e5355a8339
huguet10@gmail.com <huguet10@gmail.com@b6968054-1028-ce52-75d5-e1acad5c81b0>
2012-12-28 20:55:02 +0000

    Fin de la parte servidora

--- Servidor/treasure/treasure.model/src/main/java/com/movember/treasure/model/dao/IEstadisticaDAO.java
+++ Servidor/treasure/treasure.model/src/main/java/com/movember/treasure/model/dao/IEstadisticaDAO.java
@@ -1,10 +1,11 @@
 package com.movember.treasure.model.dao;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 public interface IEstadisticaDAO {
 
-	Integer recuperarHitosUsuIdent(Integer idRuta) throws SQLException;
+	Map<Integer,Integer> recuperarHitosUsuIdent(Integer idRuta) throws SQLException;
 
-	Integer recuperarHitosUsuNoIdent(Integer idRuta) throws SQLException;
+	Map<Integer,Integer> recuperarHitosUsuNoIdent(Integer idRuta) throws SQLException;
 }
