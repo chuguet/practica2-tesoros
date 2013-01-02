b3f3353403632159eaa3492c1603c671fe43d717
huguet10@gmail.com <huguet10@gmail.com@b6968054-1028-ce52-75d5-e1acad5c81b0>
2013-01-02 18:16:00 +0000

    Se ordena el codigo

--- trunk/Servidor/treasure/treasure.controller/src/main/java/com/movember/treasure/controller/dto/EstadisticaUsuarioDTO.java
+++ trunk/Servidor/treasure/treasure.controller/src/main/java/com/movember/treasure/controller/dto/EstadisticaUsuarioDTO.java
@@ -17,61 +17,74 @@ public class EstadisticaUsuarioDTO extends AbstractDTO {
 	/** The Constant df. */
 	private static final DecimalFormat df = new DecimalFormat("0.00");
 
-	/** The porcentaje_rutas_totales. */
-	private String porcentaje_rutas_totales;
-
-	/** The porcentaje_hitos_totales. */
-	private String porcentaje_hitos_totales;
-
-	/** The num_rutas_terminadas. */
-	private Integer num_rutas_terminadas;
-
-	public Integer getNum_rutas_terminadas() {
-		return num_rutas_terminadas;
-	}
-
-	public void setNum_rutas_terminadas(Integer num_rutas_terminadas) {
-		this.num_rutas_terminadas = num_rutas_terminadas;
-	}
-
-	public Integer getNum_hitos_terminados() {
-		return num_hitos_terminados;
-	}
-
-	public void setNum_hitos_terminados(Integer num_hitos_terminados) {
-		this.num_hitos_terminados = num_hitos_terminados;
-	}
-
-	public Integer getNum_hitos_totales() {
-		return num_hitos_totales;
-	}
-
-	public void setNum_hitos_totales(Integer num_hitos_totales) {
-		this.num_hitos_totales = num_hitos_totales;
-	}
-
-	public Integer getNum_rutas_totales() {
-		return num_rutas_totales;
-	}
-
-	public void setNum_rutas_totales(Integer num_rutas_totales) {
-		this.num_rutas_totales = num_rutas_totales;
-	}
-
 	/** The num_hitos_terminados. */
 	private Integer num_hitos_terminados;
 
 	/** The num_hitos_totales. */
 	private Integer num_hitos_totales;
 
+	/** The num_rutas_terminadas. */
+	private Integer num_rutas_terminadas;
+
 	/** The num_rutas_totales. */
 	private Integer num_rutas_totales;
+
+	/** The porcentaje_hitos_totales. */
+	private String porcentaje_hitos_totales;
 
 	/** The porcentaje_rutas_hitos. */
 	private List<RutaHitoPorcentajeDTO> porcentaje_rutas_hitos;
 
+	/** The porcentaje_rutas_totales. */
+	private String porcentaje_rutas_totales;
+
 	/** The usuario. */
 	private String usuario;
+
+	/**
+	 * Calculo porcentaje.
+	 * 
+	 * @param rutas_terminadas
+	 *            the rutas_terminadas
+	 * @param num_rutas_totales
+	 *            the num_rutas_totales
+	 * @return the string
+	 */
+	private String calculoPorcentaje(Integer rutas_terminadas,
+			Integer num_rutas_totales) {
+		Float result = new Float("0.00");
+		Float parcial = Float.valueOf(rutas_terminadas);
+		Float total = Float.valueOf(num_rutas_totales);
+		if (total.compareTo(result) > 0) {
+			result = parcial / total * 100;
+		}
+		return df.format(result).toString() + "%";
+	}
+
+	public Integer getNum_hitos_terminados() {
+		return num_hitos_terminados;
+	}
+
+	public Integer getNum_hitos_totales() {
+		return num_hitos_totales;
+	}
+
+	public Integer getNum_rutas_terminadas() {
+		return num_rutas_terminadas;
+	}
+
+	public Integer getNum_rutas_totales() {
+		return num_rutas_totales;
+	}
+
+	/**
+	 * Gets the porcentaje_hitos_totales.
+	 * 
+	 * @return the porcentaje_hitos_totales
+	 */
+	public String getPorcentaje_hitos_totales() {
+		return porcentaje_hitos_totales;
+	}
 
 	/**
 	 * Gets the porcentaje_rutas_hitos.
@@ -83,12 +96,47 @@ public class EstadisticaUsuarioDTO extends AbstractDTO {
 	}
 
 	/**
+	 * Gets the porcentaje_rutas_totales.
+	 * 
+	 * @return the porcentaje_rutas_totales
+	 */
+	public String getPorcentaje_rutas_totales() {
+		return porcentaje_rutas_totales;
+	}
+
+	/**
 	 * Gets the usuario.
 	 * 
 	 * @return the usuario
 	 */
 	public String getUsuario() {
 		return usuario;
+	}
+
+	public void setNum_hitos_terminados(Integer num_hitos_terminados) {
+		this.num_hitos_terminados = num_hitos_terminados;
+	}
+
+	public void setNum_hitos_totales(Integer num_hitos_totales) {
+		this.num_hitos_totales = num_hitos_totales;
+	}
+
+	public void setNum_rutas_terminadas(Integer num_rutas_terminadas) {
+		this.num_rutas_terminadas = num_rutas_terminadas;
+	}
+
+	public void setNum_rutas_totales(Integer num_rutas_totales) {
+		this.num_rutas_totales = num_rutas_totales;
+	}
+
+	/**
+	 * Sets the porcentaje_hitos_totales.
+	 * 
+	 * @param porcentaje_hitos_totales
+	 *            the new porcentaje_hitos_totales
+	 */
+	public void setPorcentaje_hitos_totales(String porcentaje_hitos_totales) {
+		this.porcentaje_hitos_totales = porcentaje_hitos_totales;
 	}
 
 	/**
@@ -103,6 +151,16 @@ public class EstadisticaUsuarioDTO extends AbstractDTO {
 	}
 
 	/**
+	 * Sets the porcentaje_rutas_totales.
+	 * 
+	 * @param porcentaje_rutas_totales
+	 *            the new porcentaje_rutas_totales
+	 */
+	public void setPorcentaje_rutas_totales(String porcentaje_rutas_totales) {
+		this.porcentaje_rutas_totales = porcentaje_rutas_totales;
+	}
+
+	/**
 	 * Sets the usuario.
 	 * 
 	 * @param usuario
@@ -110,6 +168,19 @@ public class EstadisticaUsuarioDTO extends AbstractDTO {
 	 */
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.treasure.controller.dto.AbstractDTO#toBusiness(java.lang
+	 * .Object)
+	 */
+	@Override
+	public void toBusiness(Object object) throws AppException {
+		// TODO Auto-generated method stub
+
 	}
 
 	/*
@@ -143,77 +214,6 @@ public class EstadisticaUsuarioDTO extends AbstractDTO {
 		this.setPorcentaje_rutas_hitos(listRutaHitoPorcentajeDTO);
 		this.usuario = estadisticaUsuario.getUsuario().getNombre() + " "
 				+ estadisticaUsuario.getUsuario().getApellidos();
-	}
-
-	/**
-	 * Calculo porcentaje.
-	 * 
-	 * @param rutas_terminadas
-	 *            the rutas_terminadas
-	 * @param num_rutas_totales
-	 *            the num_rutas_totales
-	 * @return the string
-	 */
-	private String calculoPorcentaje(Integer rutas_terminadas,
-			Integer num_rutas_totales) {
-		Float result = new Float("0.00");
-		Float parcial = Float.valueOf(rutas_terminadas);
-		Float total = Float.valueOf(num_rutas_totales);
-		if (total.compareTo(result) > 0) {
-			result = parcial / total * 100;
-		}
-		return df.format(result).toString() + "%";
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.movember.treasure.controller.dto.AbstractDTO#toBusiness(java.lang
-	 * .Object)
-	 */
-	@Override
-	public void toBusiness(Object object) throws AppException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * Gets the porcentaje_rutas_totales.
-	 * 
-	 * @return the porcentaje_rutas_totales
-	 */
-	public String getPorcentaje_rutas_totales() {
-		return porcentaje_rutas_totales;
-	}
-
-	/**
-	 * Sets the porcentaje_rutas_totales.
-	 * 
-	 * @param porcentaje_rutas_totales
-	 *            the new porcentaje_rutas_totales
-	 */
-	public void setPorcentaje_rutas_totales(String porcentaje_rutas_totales) {
-		this.porcentaje_rutas_totales = porcentaje_rutas_totales;
-	}
-
-	/**
-	 * Gets the porcentaje_hitos_totales.
-	 * 
-	 * @return the porcentaje_hitos_totales
-	 */
-	public String getPorcentaje_hitos_totales() {
-		return porcentaje_hitos_totales;
-	}
-
-	/**
-	 * Sets the porcentaje_hitos_totales.
-	 * 
-	 * @param porcentaje_hitos_totales
-	 *            the new porcentaje_hitos_totales
-	 */
-	public void setPorcentaje_hitos_totales(String porcentaje_hitos_totales) {
-		this.porcentaje_hitos_totales = porcentaje_hitos_totales;
 	}
 
 }
